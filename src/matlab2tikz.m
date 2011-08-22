a0c7a5911f20d01497b04ca6b60c4952b96cc7f0
Nico Schlömer <nico.schloemer@gmail.com>
2011-08-22 13:16:09 +0200

    updated changelog, incremented version number

--- src/matlab2tikz.m
+++ src/matlab2tikz.m
@@ -92,7 +92,7 @@ function matlab2tikz( varargin )
   m2t.currentHandles = [];
 
   m2t.name = 'matlab2tikz';
-  m2t.version = '0.1.2';
+  m2t.version = '0.1.3';
   m2t.author = 'Nico Schlömer';
   m2t.authorEmail = 'nico.schloemer@gmail.com';
   m2t.years = '2008--2011';
