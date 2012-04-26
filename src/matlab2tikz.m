a21bd90b2fae600c3d53c8e3ed9d028ec133baea
Egon Geerardyn <egon.geerardyn@gmail.com>
2012-04-26 14:34:17 +0200

    userWarning: message generation simplified
      instead of switch n with sprintf(message, var{1}, ..., var{n})
      followed by warning(...)
      used warning(...,message, varargin{:})

--- src/matlab2tikz.m
+++ src/matlab2tikz.m
@@ -4529,24 +4529,7 @@ function userWarning( m2t, message, varargin )
       return
   end
 
-  n = length(varargin);
-  switch n
-      case 0;
-         mess = sprintf( message );
-      case 1;
-         mess = sprintf( message, varargin{1} );
-      case 2;
-         mess = sprintf( message, varargin{1}, varargin{2} );
-      case 3;
-         mess = sprintf( message, varargin{1}, varargin{2}, varargin{3} );
-      case 4;
-         mess = sprintf( message, varargin{1}, varargin{2}, varargin{3}, varargin{4} );
-      otherwise
-         error( 'userWarning:longVarargin', ...
-                'Can''t deal with length(varargin)>4 yet.' );
-  end
-
-  warning( 'matlab2tikz:userWarning', mess )
+  warning( 'matlab2tikz:userWarning', message, varargin{:} );
 
 end
 % =========================================================================
