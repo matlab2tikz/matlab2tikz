962eaff35c1c62d7d60957e20e1ea9e5e8419703
Egon Geerardyn <egon.geerardyn@gmail.com>
2014-10-19 20:32:54 +0200

    More information in test output
     - OS version
     - MATLAB/Octave + version
     - pgfplots version
     - commit ID of m2t

--- test/matlab2tikz_acidtest.m
+++ test/matlab2tikz_acidtest.m
@@ -274,7 +274,20 @@ end
 % =========================================================================
 function texfile_finish(texfile_handle)
 
-  fprintf(texfile_handle, '\\end{document}');
+    [env,versionString] = getEnvironment();
+
+
+  fprintf(texfile_handle, ...
+      [
+      '\\newpage\n',...
+      '\\begin{tabular}{ll}\n',...
+      '  Created  & ' datestr(now) ' \\\\ \n', ...
+      '  OS       & ' OSVersion ' \\\\ \n',...
+      '  ' env '  & ' versionString ' \\\\ \n', ...
+      VersionControlIdentifier, ...
+      '  Pgfplots & \\expandafter\\csname ver@pgfplots.sty\\endcsname \\\\ \n',...
+      '\\end{tabular}\n',...
+      '\\end{document}']);
 
 end
 % =========================================================================
@@ -363,6 +376,23 @@ function [env,versionString] = getEnvironment()
   versionString = [];
 end
 % =========================================================================
+function [formatted, OSType, OSVersion] = OSVersion()
+    if ismac
+        OSType = 'Mac OS';
+        [dummy, OSVersion] = system('sw_vers -productVersion');
+    elseif ispc
+        OSType = 'Windows';
+        [dummy, OSVersion] = system('ver');
+    elseif isunix
+        OSType = 'Unix';
+        [dummy, OSVersion] = system('uname -r');
+    else
+        OSType = '';
+        OSVersion = '';
+    end
+    formatted = strtrim([OSType ' ' OSVersion]);
+end
+% =========================================================================
 function msg = format_error_message(e)
     msg = '';
     if ~isempty(e.message)
@@ -390,6 +420,49 @@ function disp_error_message(env, msg)
         fprintf(stderr, strrep(msg, '\', '\\'));
     else
         fprintf(stderr, msg);
+    end
+end
+% =========================================================================
+function [formatted,treeish] = VersionControlIdentifier()
+% This function gives the (git) commit ID of matlab2tikz
+%
+% This assumes the standard directory structure as used by Nico's master branch:
+%     SOMEPATH/src/matlab2tikz.m with a .git directory in SOMEPATH.
+%
+% The HEAD of that repository is determined from file system information only
+% by following dynamic references (e.g. ref:refs/heds/master) in branch files
+% until an absolute commit hash (e.g. 1a3c9d1...) is found.
+% NOTE: Packed branch references are NOT supported by this approach
+    MAXITER     = 10; % stop following dynamic references after a while
+    formatted   = '';
+    REFPREFIX   = 'ref:';
+    isReference = @(treeish)(any(strfind(treeish, REFPREFIX)));
+    treeish     = [REFPREFIX 'HEAD'];
+    try
+        % get the matlab2tikz directory
+        m2tDir = fileparts(mfilename('fullpath'));
+        gitDir = fullfile(m2tDir,'..','.git');
+
+        nIter = 1;
+        while isReference(treeish)
+            refName    = treeish(numel(REFPREFIX)+1:end);
+            branchFile = fullfile(gitDir, refName);
+
+            if exist(branchFile, 'file') && nIter < MAXITER
+                fid     = fopen(branchFile,'r');
+                treeish = fscanf(fid,'%s');
+                fclose(fid);
+                nIter   = nIter + 1;
+            else % no branch file or iteration limit reached
+                treeish = '';
+                return;
+            end
+        end
+    catch %#ok
+        treeish = '';
+    end
+    if ~isempty(treeish)
+        formatted = ['  Commit & ' treeish ' \\\\ \n'];
     end
 end
 % =========================================================================
