3fd64cf334167b229c7ad1cd1d033f3785b4339e
PeterPablo <PeterPablo@users.noreply.github.com>
2015-05-27 20:20:08 +0200

    first version of respecting user's defaults

--- test/private/initializeGlobalState.m
+++ test/private/initializeGlobalState.m
@@ -2,10 +2,27 @@ function [orig,cwd] = initializeGlobalState()
 % Initialize global state. Set working directory and various properties of
 % the graphical root to ensure reliable output of the ACID testsuite.
 % See #542 and #552
-    fprintf('Initialize global state...\n');
+%
+% 1. Working directory
+% 2. Bring get(0,'Default') in line with get(0,'Factory')
+% 3. Set specific properties, required by matlab2tikz
+    fprintf('Initialize global state...\n'); 
 
     %--- Initialize working directory
     cwd = initializeWorkingDirectory();
+
+    %--- Extract user defined default properties and set factory state
+    default = get(0,'Default');
+    factory = get(0,'Factory');
+
+    f = fieldnames(default);    % fields of user's default state
+    for i = 1:length(f)
+        factory_property_name = strrep(f{i},'default','factory');
+        factory_property_value = factory.(factory_property_name);
+        orig.(f{i}).val = ...
+            swap_property_state(0, f{i}, factory_property_value);
+    end
+    clear default factory_property_name factory_property_value
 
     %--- Define desired global state properties
     new.defaultFigurePosition.val   = [300,200,560,420];
@@ -19,7 +36,7 @@ function [orig,cwd] = initializeGlobalState()
     % not possible in octave; didn't want to duplicate private functions for now
     new.ScreenPixelsPerInch.ignore  = strcmpi(getEnvironment,'octave'); 
 
-    %--- Extract relevant properties and select desired state
+    %--- Extract relevant properties and set desired state
     f = fieldnames(new);    % fields of new state
     for i = 1:length(f)
         % ignore property on specified environments
