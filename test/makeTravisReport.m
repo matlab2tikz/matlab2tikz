3071a623597f948f0b5ebb8172cbece4c8475301
Egon Geerardyn <egon.geerardyn@gmail.com>
2015-07-29 11:24:15 +0200

    Construct GFM tables

--- test/makeTravisReport.m
+++ test/makeTravisReport.m
@@ -19,6 +19,76 @@ function nErrors = makeTravisReport(status)
     end
 end
 % ==============================================================================
+function str = gfmTable(data, header, alignment)
+    % Construct a Github-flavored Markdown table
+    %
+    % Arguments:
+    %   - data: nRows x nCols cell array that represents the data
+    %   - header: cell array with the (nCol) column headers
+    %   - alignment: alignment specification per column
+    %       * 'l': left-aligned (default)
+    %       * 'c': centered
+    %       * 'r': right-aligned
+    %     When not enough entries are specified, the specification is repeated
+    %     cyclically.
+    %
+    % Output: table as a string
+    %
+    % See https://help.github.com/articles/github-flavored-markdown/#tables
+
+    % input argument validation and normalization
+    [nRows, nCols] = size(data);
+    if ~exist('alignment','var') || isempty(alignment)
+        alignment = 'l';
+    end
+    if numel(alignment) < nCols
+        % repeat the alignment specifications to along the columns
+        alignment = repmat(alignment, 1, nCols);
+        alignment = alignment(1:nCols);
+    end
+
+    % calculate the required column width
+    cellWidth   = cellfun(@length, [header(:)' ;data]);
+    columnWidth = max(max(cellWidth, [], 1),3); % use at least 3 places
+
+    % prepare the table format
+    COLSEP = '|'; ROWSEP = sprintf('\n');
+    rowformat = [COLSEP sprintf([' %%%ds ' COLSEP], columnWidth) ROWSEP];
+    alignmentRow = formatAlignment(alignment, columnWidth);
+
+    % actually print the table
+    fullTable = [header; alignmentRow; data];
+    strs = cell(size(fullTable,1), 1);
+    for iRow = 1:numel(strs)
+        thisRow = fullTable(iRow,:);
+        %TODO: maybe preprocess thisRow with strjust first
+        strs{iRow} = sprintf(rowformat, thisRow{:});
+    end
+    str = [strs{:}];
+    function alignRow = formatAlignment(alignment, columnWidth)
+        DASH = '-'; COLON = ':';
+        N = numel(columnWidth);
+        alignRow = arrayfun(@(w) repmat(DASH, 1, w), columnWidth, ...
+                        'UniformOutput', false);
+        for iColumn = 1:N
+            thisAlign = alignment(iColumn);
+            thisSpec = alignRow{iColumn};
+            switch lower(thisAlign)
+                case 'l'
+                    thisSpec(1) = COLON;
+                case 'r'
+                    thisSpec(end) = COLON;
+                case 'c'
+                    thisSpec([1 end]) = COLON;
+                otherwise
+                    error('gfmTable:BadAlignment','Unknown alignment "%s"',...
+                          thisAlign);
+            end
+            alignRow{iColumn} = thisSpec;
+        end
+    end
+end
+% ==============================================================================
 function displaySummaryTable(stream, status)
     % display a summary table of all tests
     for iTest = 1:numel(status)
