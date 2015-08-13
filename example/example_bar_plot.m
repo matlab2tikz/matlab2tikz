bf0a0e62f92ebc1335ddc528d74b9b1e25619342
rpeschke <richard.peschke@desy.de>
2015-08-13 16:35:00 +0200

    added example file. It creates a very simple bar plot which gets converted to a standalone tex file

---
+++ example/example_bar_plot.m
@@ -0,0 +1,16 @@
+function example_bar_plot()
+test_data =[15     0; 20     0;   25     2;    30    14;    35    34;    40    57;    45    65;    50    46;    55     9;    60     2;    65     1;    70     0];
+
+% Create figure
+figure1 = figure('Color',[1 1 1]);
+
+% Create axes
+axes1 = axes('Parent',figure1);
+
+
+hb=bar(test_data(:,1),test_data(:,2),'DisplayName','Test Data');
+
+xlabel('parameter [units]');
+ylabel('#');
+legend('show','Location','northwest');
+matlab2tikz('figurehandle',figure1,'filename','example_v_bar_plot.tex' ,'standalone', true);
