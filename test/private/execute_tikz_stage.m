7e169d7ecfb0dc01feeb1e55688a620938af490f
Egon Geerardyn <egon.geerardyn@gmail.com>
2015-08-20 10:39:49 +0200

    Breakdown runtimes in tester: m2t, cleanfigure

--- test/private/execute_tikz_stage.m
+++ test/private/execute_tikz_stage.m
@@ -3,9 +3,16 @@ function [status] = execute_tikz_stage(status, ipp)
     testNumber = status.index;
     gen_tex = sprintf('data/converted/test%d-converted.tex', testNumber);
     gen_pdf  = sprintf('data/converted/test%d-converted.pdf', testNumber);
+    cleanfigure_time = NaN;
+    m2t_time = NaN;
+
     % now, test matlab2tikz
     try
+        cleanfigure_time = tic;
         cleanfigure(status.extraCleanfigureOptions{:});
+        cleanfigure_time = toc(cleanfigure_time);
+
+        m2t_time = tic;
         matlab2tikz('filename', gen_tex, ...
             'showInfo', false, ...
             'checkForUpdates', false, ...
@@ -14,6 +21,7 @@ function [status] = execute_tikz_stage(status, ipp)
             ipp.Results.extraOptions{:}, ...
             status.extraOptions{:} ...
             );
+        m2t_time = toc(m2t_time);
     catch %#ok
         e = lasterror('reset'); %#ok
         % Remove (corrupted) output file. This is necessary to avoid that the
@@ -23,4 +31,6 @@ function [status] = execute_tikz_stage(status, ipp)
     end
     status.tikzStage.texFile = gen_tex;
     status.tikzStage.pdfFile = gen_pdf;
+    status.tikzStage.m2t_time = m2t_time;
+    status.tikzStage.cleanfigure_time = cleanfigure_time;
 end
