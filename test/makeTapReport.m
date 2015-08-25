5fba117d67fa1d79e2adac18ab7f03e465d57586
Egon Geerardyn <egon.geerardyn@gmail.com>
2015-08-25 10:42:50 +0200

    Basic TAP report

---
+++ test/makeTapReport.m
@@ -0,0 +1,76 @@
+function [nErrors] = makeTapReport(status, varargin)
+% Makes a Test Anything Protocol report
+%
+% This function produces a testing report of HEADLESS tests for
+% display on Jenkins (or any other TAP-compatible system)
+%
+% MAKETAPREPORT(status) produces the report from the `status` output of
+% `testHeadless`.
+%
+% MAKETAPREPORT(status, 'stream', FID, ...) changes the filestream to use
+%  to output the report to. (Default: 1 (stdout)).
+%
+% TAP Specification: https://testanything.org
+%
+% See also: testHeadless, makeTravisReport, makeLatexReport
+
+    %% Parse input arguments
+    ipp = m2tInputParser;
+
+    ipp = ipp.addRequired(ipp, 'status', @iscell);
+    ipp = ipp.addParamValue(ipp, 'stream', 1,  @isStream);
+
+    ipp = ipp.parse(ipp, status, varargin{:});
+    arg = ipp.Results;
+
+    %% build report
+    printTAPVersion(arg.stream);
+    printTAPPlan(arg.stream, status);
+    for iStatus = 1:numel(status)
+        printTAPReport(arg.stream, status{iStatus});
+    end
+    printTAPReport(arg.stream, status);
+
+    %% set output arguments if needed
+    if nargout >= 1
+        nErrors = countNumberOfErrors(S.reliable);
+    end
+end
+% == INPUT VALIDATOR FUNCTIONS =================================================
+function bool = isStream(val)
+    % returns true if it is a valid (file) stream or stdout/stderr stream
+    bool = isnumeric(val);
+end
+% ==============================================================================
+function printTAPVersion(stream)
+    % prints the TAP version
+    fprintf(stream,'TAP version 13\n');
+end
+function printTAPPlan(stream, statuses)
+    % prints the TAP version
+    firstTest = 1;
+    lastTest = numel(statuses); % just assume we are testing everything
+    fprintf(stream,'%d..%d\n', firstTest, lastTest);
+end
+function printTAPReport(stream, status)
+    % prints a TAP test case report
+    directive = '';
+    result = 'not ok';
+    message = status.function;
+    testNum = status.index;
+
+    if status.unreliable
+        result = 'ok';
+        directive = '# TODO unreliable';
+    elseif status.skip
+        result = 'ok';
+        directive = '# SKIP skipped';
+    elseif countNumberOfErrors(status) == 0
+        result = 'ok';
+    end
+
+    fprintf(stream, '%s %d %s %s\n', result, testNum, message, directive);
+
+    %TODO: we can provide more information on the failure using YAML syntax
+end
+% ==============================================================================
