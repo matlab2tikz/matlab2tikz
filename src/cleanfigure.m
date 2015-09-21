74a93e0b1dae52035ddef4e4008aa1b3020dfb07
Michael Schellenberger Costa <mschellenbergercosta@gmail.com>
2015-09-21 14:14:30 +0200

    Move simplifyLine after pruneOutsideBox and movePointsCloser.

--- src/cleanfigure.m
+++ src/cleanfigure.m
@@ -118,11 +118,13 @@ function indent = recursiveCleanup(meta, h, targetResolution, indent)
       %display(sprintf([repmat(' ',1,indent), '  handle this']))
 
       if strcmp(type, 'line')
-          simplifyLine(meta, h, targetResolution);
           pruneOutsideBox(meta, h);
           % Move some points closer to the box to avoid TeX:DimensionTooLarge
           % errors. This may involve inserting extra points.
           movePointsCloser(meta, h);
+          % NOTE: Always remove invisible points before simplifying the
+          % line. Otherwise it will generate additional line segments
+          simplifyLine(meta, h, targetResolution);
       elseif strcmpi(type, 'stair')
           pruneOutsideBox(meta, h);
       elseif strcmp(type, 'text')
