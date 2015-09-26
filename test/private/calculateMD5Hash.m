abffa4fff6807709e63a6350f45920f1b829bd26
oleg komarov <o.komarov11@imperial.ac.uk>
2015-09-26 11:13:41 +0100

    Addressing comments

--- test/private/calculateMD5Hash.m
+++ test/private/calculateMD5Hash.m
@@ -15,16 +15,18 @@ function hash = calculateMD5Hash(filename)
             % broken. Instead we make use of the Java libraries.
             % Unless the "-nojvm" flag is specified, this should work well.
             
-            % Get message digest instance
             MD5 = java.security.MessageDigest.getInstance('MD5');
                         
             % Open the file
-            fid     = fopen(filename, 'r');
-            cleanup = onCleanup(@()fclose(fid));
+            fid = fopen(filename, 'r');
             
-            % Read file into a stream of uint8
-            stream = fread(fid, '*uint8');
-            MD5.update(stream);
+            % Make sure fid is closed
+            finally_close = onCleanup(@()fclose(fid));
+            
+            % Faster file digest based on code by Jan Simon as in 
+            % http://www.mathworks.com/matlabcentral/fileexchange/31272-datahash
+            data = fread(fid, '*uint8');
+            MD5.update(data);
             
             hash = reshape(dec2hex(typecast(MD5.digest(),'uint8')).', 1, 32);
     end
