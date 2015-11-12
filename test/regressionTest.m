4db05dc15b2059a916112d10ce12792eee8707f2
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-11-12 16:42:19 +0000

    graphical output for two commits

---
+++ test/regressionTest.m
@@ -0,0 +1,57 @@
+function regressionTest(commitBefore,commitAfter)
+    % Produce graphical output for two commits
+
+    % Work only with a clean working tree
+    [status, cmdout] = system('git status --porcelain');
+    if ~isempty(cmdout)
+        error('regressionTest:treeNotClean','Working tree is not clean.')
+    end
+
+    % Initialize state and prepare cleanup
+    [state,cwd]           = initializeGlobalState();
+    branchName            = getBranchName();
+    finally_restore_state = onCleanup(@() restoreStateAndGit(state,cwd, branchName));
+
+    % Toggle-off paging in Octave
+    if strcmpi(getEnvironment(), 'Octave')
+        more off
+    end
+
+    suite       = @ACID;
+    testIndices = 1:numel(suite(0));
+
+    makeGraphical(commitBefore, suite, testIndices);
+    makeGraphical(commitAfter , suite, testIndices);
+end
+
+function makeGraphical(commit, suite, testIndices)
+    system(['git checkout ', commit]);
+
+    testGraphical('testFunctionIndices', testIndices,...
+                  'testsuite',           suite);
+
+    % Make pdf
+    fprintf(['Making the .pdf for commit ', commit, '.\n'])
+    [status,cmdout] = system('make -j -C tex');
+
+    % Rename
+    texPath = fullfile(fileparts(which('testGraphical.m')),'tex');
+    oldName = fullfile(texPath,'acid.pdf');
+    newName = fullfile(texPath,['acid_before_' commit '.pdf']);
+    movefile(oldName, newName);
+end
+
+function branchName = getBranchName()
+    % Get branch name
+    [status,cmdout] = system('git branch');
+    branchName      = regexp(cmdout, '(?<=^* )[^\n\r]+','match','once');
+    % Deal with detached commit
+    if strcmp(branchName(1),'(')
+        branchName = regexp(branchName,'\w+(?=\))','match','once');
+    end
+end
+
+function restoreStateAndGit(state,cwd, branchName)
+    restoreGlobalState(state,cwd);
+    system(['git checkout ', branchName]);
+end
