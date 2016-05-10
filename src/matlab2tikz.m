87216a63e98d9187c39eebab0ebf9364ed763fc7
rpeschke <richard.peschke@desy.de>
2016-05-10 13:30:53 +0200

    new function which handles tick labels on color bar

--- src/matlab2tikz.m
+++ src/matlab2tikz.m
@@ -4500,6 +4500,34 @@ function fontStyle = getFontStyle(m2t, handle)
     end
 end
 % ==============================================================================
+function   cbarStyleOptions = getColotBarTicks(handle, cbarStyleOptions)
+if strcmp( get(handle,'TickLabelsMode') , 'manual')
+
+    %ColorBar TickLabels 
+    TickLabels= get(handle,'TickLabels');
+    TickLabels_option ='{';
+    for e=1:length(TickLabels)
+        TickLabels_option = [ TickLabels_option ' {' TickLabels{e} '},'];
+    end
+    TickLabels_option(end)='}';
+
+    cbarStyleOptions=cat(1,cbarStyleOptions, {'yticklabels',TickLabels_option});
+
+
+    % ColorBar Ticks Number
+    Ticks= get(handle,'Ticks');
+    Ticks_option ='{';
+    for e=1:length(Ticks)
+        Ticks_option = [ Ticks_option ' ' num2str(Ticks(e)) ','];
+    end
+    Ticks_option(end)='}';
+
+    cbarStyleOptions=cat(1,cbarStyleOptions, {'ytick',Ticks_option});
+
+
+end
+end
+% ==============================================================================
 function axisOptions = getColorbarOptions(m2t, handle)
 
     % begin collecting axes options
@@ -4561,8 +4589,11 @@ function axisOptions = getColorbarOptions(m2t, handle)
             %FIXME: sampled colorbars should be inferred, not by using strict!
         end
     end
+    %color map ticks
+   cbarStyleOptions = getColotBarTicks(handle, cbarStyleOptions);
 
-    % Merge them together in axisOptions.
+   
+   % Merge them together in axisOptions.
     axisOptions = opts_add(axisOptions, strtrim(['colorbar ', cbarTemplate]));
 
     if ~isempty(cbarStyleOptions)
